2f81d5fc5a11a28d94f3ace38c3c8bff1d0a7070
yusufwdn <53157399+yusufwdn@users.noreply.github.com>
2023-11-21 11:22:44 +0700

    Multiple Interfaces

---
+++ InterfaceMultiple/Program.cs
@@ -0,0 +1,35 @@
+﻿interface IFirstInterface
+{
+    void firstMethod(); // method interface
+}
+
+interface ISecondInterface
+{
+    void secondMethod(); // method interface
+}
+
+// melakukan implementasi dua interface
+// hal ini mengharuskan kelas MyClass membuat seluruh method yang dimiliki oleh interface 
+// yang diimplementasi
+class MyClass : IFirstInterface, ISecondInterface
+{
+    public void firstMethod()
+    {
+        Console.WriteLine("Saya adalah firstMethod dari IFirstInterface");
+    }
+
+    public void secondMethod()
+    {
+        Console.WriteLine("Saya adalah secondMethod dari ISecondInterface");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass kelasSaya = new MyClass();
+        kelasSaya.firstMethod();
+        kelasSaya.secondMethod();
+    }
+}
