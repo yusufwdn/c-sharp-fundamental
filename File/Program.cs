3fe74b3c80baa9bbfb5622e2f6f379a7085c6877
yusufwdn <53157399+yusufwdn@users.noreply.github.com>
2023-11-21 14:08:01 +0700

    File (Basic)

---
+++ File/Program.cs
@@ -0,0 +1,11 @@
+﻿// Membuat file baru yang berisi string
+
+// Membuat teks berupa string
+string writeText = "Hello world"; 
+// Membuat sebuah file dan memasukkan writeText ke dalamnya
+File.WriteAllText("filename.txt", writeText); 
+
+// Membaca konten yang ada di dalam file
+string readText = File.ReadAllText("filename.txt"); 
+// Tampilkan hasilnya di console
+Console.WriteLine(readText); 
