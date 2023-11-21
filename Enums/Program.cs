7f9c2bed7500760de7cafc04b27bc4f0bee00db5
yusufwdn <53157399+yusufwdn@users.noreply.github.com>
2023-11-21 11:50:08 +0700

    Enum

---
+++ Enums/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+    Enum
+
+    Merupakan kelas spesial yang merepresentasikan grup dari konstanta (constant, variabel yang tidak dapat dirubah).
+    Untuk membuat enum, gunakan kata kunci "enum" (baik kelas maupun interface), dan pisahkan item dari enum dengan koma.
+*/
+class Program
+{
+    enum RiskLevel 
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    static void Main(string[] args)
+    {
+        // contoh mengakses item dari enum RiskLevel
+        // caranya sama dengan ketika kita mengakses member dari kelas yang dibuat instance objek (objek.metho atau objek.property)
+        Console.WriteLine("Saat ini level resiko Anda adalah " + RiskLevel.Low + ".");
+        Console.WriteLine("Level resiko Anda mendetkati  " + RiskLevel.Medium + ".");
+    }
+
+}
